214c322a23e64359b70149772d4d9d57f52f2da3
Baeed khan <baeedk7@gmail.com>
2025-02-18 01:26:24 +0500

    JavaScript Assingment

---
+++ JS Assingment 01/script.js
@@ -0,0 +1,121 @@
+// 1. Write a program to take “city” name as input from user. If
+// user enters “Karachi”, welcome the user like this:
+// “Welcome to city of lights”
+
+// var userInput  = prompt("Enter the City Name");
+
+// if(userInput === "Karachi"){
+//     console.log("Welcome to the City of Lights")
+//     alert("Welcome to the City of Lights")
+// }
+// else{
+//     alert("Write Karachi to execute it")
+//     console.log("Write Karachi to execute it")
+// }
+
+
+// 2. Write a program to take “gender” as input from user. If the
+// user is male, give the message: Good Morning Sir. If the
+// user is female, give the message: Good Morning Ma’am.
+
+// var userInput = prompt("Enter your Gender")
+
+// if (userInput === "male") {
+//     alert("Good Morning Sir")
+//     console.log("Good Morning Sir")
+// }
+// else if (userInput === "female") {
+//     alert("Good Morning Ma'am")
+//     console.log("Good Morning Ma'am")
+
+// }
+
+
+// 3. Write a program to take input color of road traffic signal
+// from the user & show the message according to this table:
+
+// var userInput = prompt("Give the Color of Road Traffic Signals")
+
+// if (userInput === "red") {
+//     alert("Must Stop")
+//     console.log("Must Stop")
+// }
+// else if (userInput === "yellow") {
+//     alert("Get Ready to Move")
+//     console.log("Get Ready to Move")
+// }
+// else if (userInput === "green") {
+//     alert("Move Now")
+//     console.log("Move Now")
+// }
+// else {
+//     alert("Invalid Color")
+//     console.log("Invalid Color")
+// }
+
+
+// 4. Write a program to take input remaining fuel in car (in
+//     litres) from user. If the current fuel is less than 0.25litres,
+//     show the message “Please refill the fuel in your car”
+
+// var userInput = Number(prompt("Remaining Feul in Car in Numbers"));
+
+// if (userInput < 0.25) {
+//     alert("Please Refuel your car")
+//     console.log("Please Refuel your car")
+
+// }
+// else {
+//     alert("You have sufficient fuel in your car")
+//     console.log("You have sufficient fuel in your car")
+//     }
+
+// question 5 skipped
+
+
+
+// 6. Write a program to take input the marks obtained in three
+// subjects & total marks. Compute & show the resulting
+// percentage on your page. Take percentage & compute
+// grade as per following table:
+
+// var obtainMarks = Number(prompt("Obtain Marks"))
+// var totalMarks = Number(prompt("Total Marks"))
+// var percentage = (obtainMarks / totalMarks) * 100
+
+
+// console.log("Mark Sheet");
+
+// if (percentage >= 80) {
+//     console.log(`Total Marks: ${totalMarks}`);
+//     console.log(`Marks Obtained: ${obtainMarks}`);
+//     console.log(`Percentage: ${percentage}`);
+//     console.log("Grade: A-One");
+//     console.log("Remarks : Excellent");
+
+// } else if (percentage >= 70) {
+//     console.log(`Total Marks: ${totalMarks}`);
+//     console.log(`Marks Obtained: ${obtainMarks}`);
+//     console.log(`Percentage: ${percentage}`);
+//     console.log("Grade: A")
+//     console.log("Remarks : Good");
+// }
+
+// else if (percentage >= 60) {
+//     console.log(`Total Marks: ${totalMarks}`);
+//     console.log(`Marks Obtained: ${obtainMarks}`);
+//     console.log(`Percentage: ${percentage}`);
+//     console.log("Grade: B")
+//     console.log("Remarks : Your Need to Improve");
+// }
+
+// else if (percentage < 60) {
+//     console.log(`Total Marks: ${totalMarks}`);
+//     console.log(`Marks Obtained: ${obtainMarks}`);
+//     console.log(`Percentage: ${percentage}`);  
+//     console.log("Fail")
+//     console.log("Remarks : Sorry");
+// }
+
+// alert("Mark Sheet Has been Showed in Console");
+
